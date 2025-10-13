f3408412e7867ec1612edd1a2272924d866443a8
Soph <sophearle16@gmail.com>
2025-10-13 19:04:59 +0100

    temp fixed wind blast for perspective camera
    it janky though not work properly

--- Assets/Scripts/Player/Wind Blast/FirePoint.cs
+++ Assets/Scripts/Player/Wind Blast/FirePoint.cs
@@ -5,7 +5,12 @@ public class FirePoint : MonoBehaviour
     //to run just before a projectile is instantiated
     public void SetupFiring()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ////ONLY WORKS WITH ORTHOGRAPHIC CAMERA
+        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position) - Vector3.up;
+        Vector3 mousePosition = Input.mousePosition - playerScreenPoint; //player to mouse
+
         //get z angle from x and y
         float angleRadius = Mathf.Atan2(mousePosition.y - transform.parent.position.y, mousePosition.x - transform.parent.position.x);
         float angleDegrees = 180 / Mathf.PI * angleRadius - 90; //offset this by 90 degrees
