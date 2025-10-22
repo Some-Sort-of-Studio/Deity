966d579dc116d3c65ad3f8ee03e0b56e4dea6765
Snowballgamzzz <2winga47@solent.ac.uk>
2025-10-22 13:24:40 +0100

    Test level load
    I have added a script that will allow the player to load the test level during the build. I have not tested

--- Assets/Scripts/Player/LoadTestLevel.cs
+++ Assets/Scripts/Player/LoadTestLevel.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadTestLevel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.L))
+        {
+            SceneManager.LoadScene("player ability testing scene");
+        }
     }
 }
