1fe38a66e1661f74593844ae4d40608fa9c4f14b
MYAUNTIESPASSIVEAGRESSIVE <adamkw123lightning@gmail.com>
2025-10-24 17:46:20 +0100

    Room Transition Amend

--- Assets/Scripts/Camera/Room Transition/Room Transition.cs
+++ Assets/Scripts/Camera/Room Transition/Room Transition.cs
@@ -4,53 +4,21 @@ using UnityEngine;
 public class RoomTransition : MonoBehaviour
 {
     [Header("Room Data")]
-    public GameObject PrevRoom = null;
     public GameObject CurrentRoom;
-    public GameObject NextRoom;
-
-    private CinemachineBrain mainBrain;
-
-    private void Start()
-    {
-        mainBrain = GetComponent<CinemachineBrain>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("RoomBox"))
+        if (collision.CompareTag("Player"))
         {
-            CurrentRoom = collision.gameObject;
-
-            if(collision.gameObject == NextRoom)
-            {
-                TransitionCamera(collision.gameObject);
-            }
-
-            if(PrevRoom != null && collision.gameObject == PrevRoom)
-            {
-                TransitionCamera(collision.gameObject);
-            }
-
-            CheckNextRoom(CurrentRoom);
+            CurrentRoom.SetActive(true);
         }
-    }
-
-    void TransitionCamera(GameObject room)
-    {
-        CinemachineCamera virtCam = room.GetComponentInChildren<CinemachineCamera>();
-        virtCam.gameObject.SetActive(true);
-        NextRoom = CurrentRoom;
-        CurrentRoom = room;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        CinemachineCamera virtCam = PrevRoom.GetComponentInChildren<CinemachineCamera>();
-        virtCam.gameObject.SetActive(false);
-    }
-
-    void CheckNextRoom(GameObject room)
-    {
-
+        if (collision.CompareTag("Player"))
+        {
+            CurrentRoom.SetActive(false);
+        }
     }
 }
