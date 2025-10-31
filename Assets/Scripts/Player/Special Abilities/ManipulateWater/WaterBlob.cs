2c365661c09cd4e3a54f90491244266ff1d17c14
soph <sophearle16@gmail.com>
2025-10-31 14:06:39 +0000

    water dissipating effects

--- Assets/Scripts/Player/Special Abilities/ManipulateWater/WaterBlob.cs
+++ Assets/Scripts/Player/Special Abilities/ManipulateWater/WaterBlob.cs
@@ -1,14 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class WaterBlob : MonoBehaviour
 {
     public Water waterSource;
-
     public Vector3 destPosition { set; private get; }
     public float drainAmount { set; private get; }
 
+    [SerializeField] private float lifetime = 10;
+    private bool goingToDest;
+
+    private void Awake()
+    {
+        goingToDest = true;
+        Destroy(gameObject, lifetime);
+    }
+
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, destPosition, 0.01f);
+        if (goingToDest)
+        {
+            transform.position = Vector3.Lerp(transform.position, destPosition, 0.01f);
+
+            if (Vector3.Distance(transform.position, destPosition) < 0.1f)
+            {
+                goingToDest = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.down * 0.0005f);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        waterSource.Fill(drainAmount);
     }
 }
