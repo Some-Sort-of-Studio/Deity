bbe6fe9f7796cd19774e11ba26da041d40153872
MYAUNTIESPASSIVEAGRESSIVE <adamkw123lightning@gmail.com>
2025-11-03 02:10:36 +0000

    Settings Menu Work

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] private Dropdown resDropDown;
+    Resolution[] resolutions;
 
+    private void Start()
+    {
+        SetFullScreen(true);
+        resolutions = Screen.resolutions;
+
+        List<string> options = new List<string>();
+
+        foreach (Resolution resolution in resolutions)
+        {
+            options.Add(resolution.ToString());
+        }
+
+        resDropDown.AddOptions(options);
+    }
+
+    public void SetFullScreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
 }
