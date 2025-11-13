b16f421322a6eea318e34c2ea2171ba612b1eba5
MYAUNTIESPASSIVEAGRESSIVE <adamkw123lightning@gmail.com>
2025-11-13 23:22:13 +0000

    Inventory work

--- Assets/Scripts/Player/InventorySlot.cs
+++ Assets/Scripts/Player/InventorySlot.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class InventorySlot : MonoBehaviour
 {
-    public Sprite IconSprite;
+    private UnityEngine.UI.Image IconSprite;
+    public string TomeTitle;
+    private Tome SlotTome;
+    [SerializeField] private GameObject TomeViewer;
+
+    public void AddToSlot(Tome tome)
+    {
+        SlotTome = tome;
+        IconSprite.sprite = tome.TomeIcon;
+        TomeTitle = tome.TomeName;
+    }
+
+    public void ViewTome()
+    {
+        TomeViewer.SetActive(true);
+    }
 }
