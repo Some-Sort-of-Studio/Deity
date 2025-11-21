6848491fbba0070ad33c1dfe2f3fce3ac2b046bc
dion <lordodin92@googlemail.com>
2025-11-21 13:55:05 +0000

    halfway done

--- Assets/Scripts/Environment/ArrowDispenser/Arrow.cs
+++ Assets/Scripts/Environment/ArrowDispenser/Arrow.cs
@@ -5,22 +5,62 @@ public class Arrow : MonoBehaviour
     public GameObject firePoint;
     public GameObject projectile;
 
-    private bool hasFired;
+    public bool hasFired;
+    public bool isActive;
 
     public float spawnTimer;
+    public float delayTimer;
+
+    public enum States
+    {
+        On,
+        Off
+    }
+
+    public States state;
 
     public void Update()
     {
-        if (!hasFired)
+        if (isActive)
         {
             Invoke("SpawnObject", spawnTimer);
-            hasFired = true;
         }
     }
 
     private void SpawnObject()
     {
         Instantiate(projectile, firePoint.transform.position, firePoint.transform.rotation);
-        hasFired = false;
+        Invoke("DelayShot", delayTimer);
+    }
+
+    private void DelayShot()
+    {
+        Invoke("SpawnObject", spawnTimer);
+    }
+
+    public void On()
+    {
+        isActive = true;
+    }
+
+    public void Off()
+    {
+        isActive = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isActive = false;
+        }
     }
 }
