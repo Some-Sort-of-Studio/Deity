15f0b0daf28b529cc9d74b35f784d47ee4db5281
Soph <sophearle16@gmail.com>
2025-12-06 15:01:06 +0000

    water draining doesnt go through floor and crow homes

--- Assets/Scripts/Player/Special Abilities/ManipulateWater/Water.cs
+++ Assets/Scripts/Player/Special Abilities/ManipulateWater/Water.cs
@@ -7,25 +7,31 @@ public class Water : MonoBehaviour
     [HideInInspector] public bool empty;
     [HideInInspector] public bool full;
 
+    private Vector3 originalScale;
+
     private void Awake()
     {
         startPosition = transform.position;
 
         empty = false;
         full = true;
+
+        originalScale = transform.localScale;
     }
 
     public void Drain(float amount)
     {
-        transform.position += Vector3.down * amount;
+        transform.position += Vector3.down * amount/2;
+        transform.localScale += Vector3.down * amount;
 
         full = false;
-        empty = transform.position.y <= (startPosition.y - transform.localScale.y);
+        empty = transform.position.y <= (startPosition.y - originalScale.y/2);
     }
 
     public void Fill(float amount)
     {
-        transform.position += Vector3.up * amount;
+        transform.position += Vector3.up * amount/2;
+        transform.localScale += Vector3.up * amount;
 
         empty = false;
         if (transform.position.y < startPosition.y) { full = false; }
