8f6d47dcdcc4e203c24487a4fbd386f63062be14
soph <sophearle16@gmail.com>
2025-12-09 15:55:49 +0000

    haha

--- Assets/Scripts/Environment/TooltipIfCharacter.cs
+++ Assets/Scripts/Environment/TooltipIfCharacter.cs
@@ -8,14 +8,19 @@ public class TooltipIfCharacter : MonoBehaviour
         Octopus
     }
 
-    GameObject playerObject;
+    [SerializeField] private GameObject playerObject;
     [SerializeField] private WhichCharacter whichCharacter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Invoke("CheckForPlayer", 2);
+    }
+
+    private void CheckForPlayer()
+    {
         playerObject = GameObject.FindFirstObjectByType<PlayerMovement2D>().gameObject;
-        if((playerObject.GetComponent<WindBlast>() && whichCharacter == WhichCharacter.Octopus)
+        if ((playerObject.GetComponent<WindBlast>() && whichCharacter == WhichCharacter.Octopus)
             || (playerObject.GetComponent<ManipulateWater>() && whichCharacter == WhichCharacter.Bird))
         {
             Destroy(gameObject);
